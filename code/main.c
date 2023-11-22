c734172f0396d345ae9d074833e1d8da5d825528
Gabriel Melo <116776177+gabrieljtmelo@users.noreply.github.com>
2023-11-22 00:02:15 +0000

    update main.c
    deleted an include

--- code/main.c
+++ code/main.c
@@ -2,7 +2,7 @@
 *  @file main.c
  * @author Gabriel Melo
  * @date March, 2023
- * @brief Jogo "NIM" usuário vs computador
+ * @brief Jogo "NIM" usuï¿½rio vs computador
  *
  * 
  *  
@@ -14,7 +14,7 @@
 #include <time.h>
 #include <conio.h>
 #include "header.h"
-#include <unistd.h>
+
 
 void main(void){
     
@@ -31,18 +31,18 @@ void main(void){
     // Loop para Menu interativo
     while (menu != 4)
     {
-        // Escolha da quantidade de peças
+        // Escolha da quantidade de peï¿½as
         while(n_max < 2){
-            printf("\n\nBem vindo à incrível jogativa de NIM!!\n");
-            printf("Estabeleça um número máximo de peças maior ou igual a 2\n");
-            printf("Relembrando que este número será o teto, o número de peças será menor ou igual a este máximo pré-estabelecido\n");
-            printf("Número de peças: ");
+            printf("\n\nBem vindo ï¿½ incrï¿½vel jogativa de NIM!!\n");
+            printf("Estabeleï¿½a um nï¿½mero mï¿½ximo de peï¿½as maior ou igual a 2\n");
+            printf("Relembrando que este nï¿½mero serï¿½ o teto, o nï¿½mero de peï¿½as serï¿½ menor ou igual a este mï¿½ximo prï¿½-estabelecido\n");
+            printf("Nï¿½mero de peï¿½as: ");
             test = scanf("%d", &n_max);
 
             // Caso o input estja errado
             if(n_max < 2 || test != 1){
                 scanf("%*[^\n]");
-                printf("\nEssa não é uma escolha possível");
+                printf("\nEssa nï¿½o ï¿½ uma escolha possï¿½vel");
                 sleep(1);
                 printf("\nResetando");
                 sleep(1);
@@ -64,8 +64,8 @@ void main(void){
         while(menu!=4)
         {
             printf("\n\nSelecione o modo de jogo\n");
-            printf("Digite 1 para começar jogando contra o computador\n");
-            printf("Digite 2 para deixar o computador começar\n");
+            printf("Digite 1 para comeï¿½ar jogando contra o computador\n");
+            printf("Digite 2 para deixar o computador comeï¿½ar\n");
             printf("Digite 3 para jogar contra um outro jogador local\n");
             printf("Digite 4 para sair\n");
             printf("Escolha: ");
@@ -73,7 +73,7 @@ void main(void){
             
             // Caso o input esteja errado
             if(menu < 1 || menu > 4 || test != 1){
-                printf("\nEssa não é uma escolha possível");
+                printf("\nEssa nï¿½o ï¿½ uma escolha possï¿½vel");
                 scanf("%*[^\n]");
                 sleep(1);
                 printf("\nVoltando");
@@ -88,15 +88,15 @@ void main(void){
             else{
                 switch (menu)
                 {
-                // Jogador Começa
+                // Jogador Comeï¿½a
                 case 1:
                     while(n > 0){
-                        printf("\nO total de peçaas é %d. O máximo de peças possíveis q pode-se retirar é %d\n", n, m);
-                        printf("Número de peças a se retirar: ");
+                        printf("\nO total de peï¿½aas ï¿½ %d. O mï¿½ximo de peï¿½as possï¿½veis q pode-se retirar ï¿½ %d\n", n, m);
+                        printf("Nï¿½mero de peï¿½as a se retirar: ");
                         test = scanf("%d", &escolha);
-                        // Caso a escolha seja maior que o número permitido de peças, ou menor ou igual a zero
+                        // Caso a escolha seja maior que o nï¿½mero permitido de peï¿½as, ou menor ou igual a zero
                         if(escolha > m || escolha <= 0 || test != 1){
-                            printf("\nSua escolha não atende as regras\n");
+                            printf("\nSua escolha nï¿½o atende as regras\n");
                             scanf("%*[^\n]");
                         } 
                         else {
@@ -104,27 +104,27 @@ void main(void){
                             if(n > 0){
                                 x = Computer_Move(&n, m, &winner);
                                 sleep(1);
-                                printf("\nO computador retirou %d peças, ficando assim, %d peças restando\n", x, n);
+                                printf("\nO computador retirou %d peï¿½as, ficando assim, %d peï¿½as restando\n", x, n);
                             }
                         }
                     }
                     break;
 
-                // Computador Começa (Funcionando :emojidasmaozinhasjuntas:)
+                // Computador ComeÃ§a (Funcionando :emojidasmaozinhasjuntas:)
                 case 2:
-                    printf("\nO total de peçaas é %d. O máximo de peças possíveis q pode-se retirar é %d\n", n, m);
+                    printf("\nO total de peï¿½aas ï¿½ %d. O mï¿½ximo de peï¿½as possï¿½veis q pode-se retirar ï¿½ %d\n", n, m);
                     while(n > 0){
                         x = Computer_Move(&n, m, &winner);
                         sleep(1);
-                        printf("\nO computador retirou %d peças, ficando assim, %d peças restando\n", x, n);
+                        printf("\nO computador retirou %d peï¿½as, ficando assim, %d peï¿½as restando\n", x, n);
                         if(n > 0){
-                            printf("\nO total de peças é %d. O máximo de peças possíveis q pode-se retirar é %d\n", n, m);
-                            printf("Número de peças a se retirar: ");
+                            printf("\nO total de peï¿½as ï¿½ %d. O mï¿½ximo de peï¿½as possï¿½veis q pode-se retirar ï¿½ %d\n", n, m);
+                            printf("Nï¿½mero de peï¿½as a se retirar: ");
                             test = scanf("%d", &escolha);
                             
-                            // Caso a escolha seja maior que o número permitido de peças, ou menor ou igual a zero
+                            // Caso a escolha seja maior que o nï¿½mero permitido de peï¿½as, ou menor ou igual a zero
                             if(escolha > m || escolha <= 0 || test != 1){
-                                printf("\nSua escolha não atende as regras\n");
+                                printf("\nSua escolha nï¿½o atende as regras\n");
                                 scanf("%*[^\n]");
                             }
                             else Player_Move(&n, escolha, &winner);
@@ -135,14 +135,14 @@ void main(void){
                 // Jogador contra outro Jogador
                 case 3:
                     while(n > 0){
-                        printf("\n\nO total de peçaas é %d. O máximo de peças possíveis q pode-se retirar é %d\n", n, m);
+                        printf("\n\nO total de peï¿½aas ï¿½ %d. O mï¿½ximo de peï¿½as possï¿½veis q pode-se retirar ï¿½ %d\n", n, m);
                         printf("Vez do JOGADOR 1\n");
-                        printf("Número de peças a se retirar: ");
+                        printf("Nï¿½mero de peï¿½as a se retirar: ");
                         // Escolha do JOGADOR 1
                         test = scanf("%d", &escolha);
-                        // Caso a escolha seja maior que o número permitido de peças, ou menor ou igual a zero
+                        // Caso a escolha seja maior que o nï¿½mero permitido de peï¿½as, ou menor ou igual a zero
                         if(escolha > m || escolha <= 0 || test != 1){
-                            printf("\nSua escolha não atende as regras\n");
+                            printf("\nSua escolha nï¿½o atende as regras\n");
                             scanf("%*[^\n]");
                         }
                         else {
@@ -150,13 +150,13 @@ void main(void){
                             if(n > 0){
                                 escolha = -1;
                                 while(escolha > m || escolha <= 0 || test != 1){
-                                    printf("\n\nO total de peçaas é %d. O máximo de peças possíveis q pode-se retirar é %d\n", n, m);
+                                    printf("\n\nO total de peï¿½aas ï¿½ %d. O mï¿½ximo de peï¿½as possï¿½veis q pode-se retirar ï¿½ %d\n", n, m);
                                     printf("Vez do JOGADOR 2\n");
-                                    printf("Número de peças a se retirar: ");
+                                    printf("Nï¿½mero de peï¿½as a se retirar: ");
                                     // Escolha do JOGADOR 2
                                     test = scanf("%d", &escolha);
                                     if(escolha > m || escolha <= 0 || test != 1){
-                                        printf("\nSua escolha não atende as regras\n");
+                                        printf("\nSua escolha nï¿½o atende as regras\n");
                                         sleep(1);
                                         scanf("%*[^\n]");
                                     }
@@ -174,10 +174,10 @@ void main(void){
                 return;
                 }
                 // Fim do Loop e resultados finais
-                if(winner == 'A') printf("\n\nParabéns, jogador 1! Você derrotou o seu oponente\n");
-                else if(winner == 'B') printf("\n\nParabéns, jogador 2! Você derrotou o seu oponente\n");
+                if(winner == 'A') printf("\n\nParabï¿½ns, jogador 1! Vocï¿½ derrotou o seu oponente\n");
+                else if(winner == 'B') printf("\n\nParabï¿½ns, jogador 2! Vocï¿½ derrotou o seu oponente\n");
                 else if(winner == 'C') printf("\n\nInfelizmente, o computador te venceu nessa :(\n");
-                else if(winner = 'P') ("\n\nParabéns, você derrotou o computador!\n");
+                else if(winner = 'P') ("\n\nParabï¿½ns, vocï¿½ derrotou o computador!\n");
                 else printf("\n\nERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRO!!!!!!!!!!\n\n");
                 menu = 4;
                 }            
